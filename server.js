1a23a6e0a61e72377ae1ac6e0c0edb7431948308
MarkoP3 <35138960+MarkoP3@users.noreply.github.com>
2018-01-05 13:49:11 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var i=0;
+const app = express();
+app.set('view engine','ejs');
+app.get('/',function (req, res) { res.render('index');});
+app.use('/css',express.static('css'));
+app.use('/img',express.static('img'));
+app.use('/js',express.static('js'));
+app.listen(3000,function (){console.log("Radim");});
