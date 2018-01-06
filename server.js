885b7b7797fe5d6e1ce499b482111c5815bf69bf
MarkoP3 <35138960+MarkoP3@users.noreply.github.com>
2018-01-06 19:19:21 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var i=0;
-const app = express();
-app.set('view engine','ejs');
-app.get('/',function (req, res) { res.render('index');});
-app.use('/css',express.static('css'));
-app.use('/img',express.static('img'));
-app.use('/js',express.static('js'));
-app.listen(8080,function (){console.log("Radim");});
