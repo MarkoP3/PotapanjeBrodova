61ba8a6d5057bd986bf99ee3e3cfb23b91e01b8f
MarkoP3 <35138960+MarkoP3@users.noreply.github.com>
2018-09-18 12:57:00 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+var server=app.listen(8080);
+var sockets=[];
+var games = {player1:"", player1data:"", player2: "", player2data: "",player1hit:0,player2hit:0};
+var gamesready=[];
+var io=require('socket.io').listen(server);
+app.get('/', function (zahtev,odgovor) {
+   odgovor.sendFile(__dirname+'/index.html');
+});
+app.use('/public',express.static('public'));
+
+io.on('connection', function (socket) {
+  sockets.push(socket.id);
+    socket.on("findGame",function(data)
+    {
+        if(games.player1=="") {
+          games.player1=socket.id;
+            games.player1data=data;
+        }
+        else
+        {
+            games.player2=socket.id;
+            games.player2data=data;
+            gamesready.push(games);
+            io.to(games.player1).to(games.player2).emit("gameFound",{gameid:gamesready.indexOf(games),turn:games.player1});
+            games = {player1:"", player1data:"", player2: "", player2data: ""};
+
+        }
+    });
+socket.on("attack", function (data) {
+   if(gamesready[data.gameid].player1==socket.id)
+   {
+       if(gamesready[data.gameid].player2data[data.block]==1)
+       {
+           io.to(gamesready[data.gameid].player1).to(gamesready[data.gameid].player2).emit("hit",{player:socket.id,ship:data.block,type:"hit"});
+        gamesready[data.gameid].player1hit++;
+           if(gamesready[data.gameid].player1hit==17)
+           {
+               io.to(gamesready[data.gameid].player1).to(gamesready[data.gameid].player2).emit("endGame",{winner:socket.id});
+               gamesready.splice(gamesready[data.gameid]);
+           }
+       }
+       else
+       {
+           io.to(gamesready[data.gameid].player1).to(gamesready[data.gameid].player2).emit("hit",{player:socket.id,ship:data.block,type:"miss"});
+       }
+   }
+    else
+   {
+       if(gamesready[data.gameid].player1data[data.block]==1)
+       {
+           io.to(gamesready[data.gameid].player1).to(gamesready[data.gameid].player2).emit("hit",{player:socket.id,ship:data.block,type:"hit"});
+           gamesready[data.gameid].player2hit++;
+           if(gamesready[data.gameid].player2hit==17)
+           {
+               io.to(gamesready[data.gameid].player1).to(gamesready[data.gameid].player2).emit("endGame",{winner:socket.id});
+               gamesready.splice(gamesready[data.gameid]);
+           }
+       }
+       else
+       {
+           io.to(gamesready[data.gameid].player1).to(gamesready[data.gameid].player2).emit("hit",{player:socket.id,ship:data.block,type:"miss"});
+       }
+   }
+});
+    socket.on("disconnect", function () {
+ sockets.splice(sockets.indexOf(socket.id),1);
+    });
+});
